cef84a6151713f0f86d461548720ec9489e8ecd5
Geane Vieira <geane.svieira@gmail.com>
2022-07-26 09:49:06 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -21,9 +20,42 @@ public class Main {
         mentoria.setDescricao("Descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devGeane = new Dev();
+        devGeane.setNome("Geane");
+        devGeane.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos" + devGeane.getConteudosInscritos());
+        devGeane.progredir();
+        devGeane.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos" + devGeane.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos" + devGeane.getConteudosConcluidos());
+        System.out.println("XP: " + devGeane.calcularTotalXp());
+
+        System.out.println("--------");
+
+        Dev devIvan = new Dev();
+        devIvan.setNome("Ivan");
+        devIvan.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos" + devGeane.getConteudosInscritos());
+        devIvan.progredir();
+        devIvan.progredir();
+        devIvan.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos" + devGeane.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos" + devIvan.getConteudosConcluidos());
+        System.out.println("XP: " + devIvan.calcularTotalXp());
+
 
     }
 
